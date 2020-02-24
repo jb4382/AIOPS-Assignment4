299a44b9c80455be51f819774783e795ed084841
Hanyu Wu <hw2753@columbia.edu>
2020-02-23 21:55:53 -0500

    Add environment setting file

---
+++ env.sh
@@ -0,0 +1,4 @@
+export SM_CHANNEL_TRAIN="/Users/HarryWoo/Git/columbia-aiops-training/model_training/SM_CHANNEL_TRAIN"
+export SM_CHANNEL_VALIDATION="/Users/HarryWoo/Git/columbia-aiops-training/model_training/SM_CHANNEL_VALIDATION"
+export SM_CHANNEL_EVAL="/Users/HarryWoo/Git/columbia-aiops-training/model_training/SM_CHANNEL_EVAL"
+export SM_MODEL_DIR="/Users/HarryWoo/Git/columbia-aiops-training/model_training/SM_MODEL_DIR"
